55d32591e93527b8c2a7cdac7ad598933888f0c6
catanielena <catani.elena1@gmail.com>
2021-07-29 14:53:43 +0200

    Add: js-ticket-fare

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// 1. Numero km che si vuole percorrere
+var kmToTravel = parseInt( prompt('Inserisci i km da percorrere'));
+// 2. Età del passeggero
+var passengerAge = parseInt(prompt('Inserisci l\'età del passeggero'));
+// 3. Prezzo del biglietto in base ai km (0.21 € al km)
+var fare = kmToTravel * 0.21;
+// 4. Condizioni di sconto (20% per i minorenni, 40% per gli over 65)
+if (passengerAge < 18) {
+    fare -= fare * 0.2;
+} else if (passengerAge > 65) {
+    fare -= fare * 0.4;
+}
+// 5. Output
+fare = fare.toFixed(2) + "€";
+document.getElementById('fare').innerHTML += fare;
